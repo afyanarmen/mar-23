383338c4f6312a31e6d12af5203849b4d619af46
Narek Ghevandiani <n.ghevandiani@gmail.com>
2018-03-20 22:46:36 +0400

    class work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
+import P from './components/pureComponent';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      a: 1
+    };
+  }
+  change = () => {
+    const state = {...this.state};
+    state.a++;
+
+    this.setState(state);
+  }
   render() {
     return (
       <div className="App">
-        <p>Hello React World</p>
+        <P
+          r={this.state.a}
+          change={this.change}
+        />
       </div>
     );
   }
